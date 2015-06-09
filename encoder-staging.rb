1e29a416baf177fd83fc55e53d312b366bc7dfc7
stegosaur <doron@ec2-54-86-157-204.compute-1.amazonaws.com>
2015-06-09 00:11:48 +0000

    sweeper and more revisions

--- encoder-staging.rb
+++ encoder-staging.rb
@@ -16,7 +16,7 @@ def getInstances()
     @oldEncoders = @encoders unless @encoders.nil?
     @encoders = []
     @instances.each{ |instance| 
-        if instance.inspect.match(/enc.*?-staging/) and !(instance.inspect.match(/enc0/))
+        if instance.inspect.match(/enc.*?-staging/) and !(instance.inspect.match(/enc0-staging/))
             @encoders << { "#{instance.inspect.match(/enc.*?-staging/)}" => "#{instance[4][0][0]}"} 
             @zone[instance[4][0][11][0]] += 1
         end
@@ -66,19 +66,32 @@ def monitor(encoder, active)
     end
 end
 
-def sweeper() #this will
+def sweeper() #this will prevent jobs from getting stuck on an offline transcoder
+    registeredTranscoders = []
+    @db.query("select distinct(transcoder_id) from transcoder").each {|result| registeredTranscoders <<  result.values[0] unless result.values[0] == "enc0-staging"}
+    registeredTranscoders.each {|transcoder|
+        running = @ec2.describe_instance_status(:instance_ids => [transcoder.gsub("enc", "i-").gsub("-staging", "")]).data.inspect.match(/running/).to_s rescue Aws::EC2::Errors::InvalidInstanceIDNotFound
+        if running != "running"
+            @db.query("delete from transcoder where transcoder_id='#{transcoder}'")
+            @logger.error("deleted offline transcoder #{transcoder} from pool")
+            @db.query("update queue set processed = null where transcoder_id='#{transcoder}'")
+            @db.query("update queue set transcoder_id='enc0-staging' where transcoder_id='#{transcoder}'")
+            @db.query("update uploadqueue set upload_server_id='enc0-staging' where upload_server_id='#{transcoder}'")
+            @db.query("update uploadqueue set processing = NULL where upload_server_id='#{transcoder}'")
+            @logger.error("moved jobs from offline transcoder #{transcoder}")
+        end }
 end 
 
 def stop_encoder(encoder) 
     success=false
-    timeStarted=@ec2.describe_instances(:instance_ids => [encoder.values])[:reservations][0][:instances][0][10]
+    timeStarted=@ec2.describe_instances(:instance_ids => encoder.values)[:reservations][0][:instances][0][10]
     if @db.query("select (select count(*) from uploadqueue where upload_server_id='#{encoder.keys.first}') + (select count(*) from queue where transcoder_id='#{encoder.keys.first}') as total").first.values[0] == 0
         if @ec2.describe_instance_status(:instance_ids => encoder.values).data.inspect.match(/running/) 
             if  (timeStarted.min-15...timeStarted.min).cover?(Time.now.min) || (timeStarted.min-15+60..timeStarted.min+60).cover?(Time.now.min) 
                 @db.query("delete from transcoder where transcoder_id='#{encoder.keys.first}'")
                 @db.query("delete from uploader where uploader_id='#{encoder.keys.first}'")
                 @logger.info("stopping #{encoder.keys.first}")
-                @ec2.stop_instances(:instance_ids => encoder.values)
+                @ec2.terminate_instances(:instance_ids => encoder.values)
                 success = true
                 monitor(encoder, false)
             else
@@ -88,12 +101,10 @@ def stop_encoder(encoder)
     else
         in_service=@db.query("select in_service from transcoder where transcoder_id='#{encoder.keys.first}'").first
         if !(@ec2.describe_instance_status(:instance_ids => encoder.values).data.inspect.match(/running/) )
-            @logger.error("#{encoder.keys.first} is down but jobs are queued, moving to enc0")
-            @db.query("update queue set transcoder_id='enc0-staging' where transcoder_id='#{encoder.keys.first}'")
-            @db.query("update uplodaqueue set upload_server_id='enc0-staging' where transcoder_id='#{encoder.keys.first}'")
+            @logger.error("#{encoder.keys.first} is down but jobs are queued")
         else
             @logger.info("job stuck on #{encoder.keys.first}, skipping")
-            if timeStarted[encoder.keys.first].nil? || (timeStarted[encoder.keys.first].min-20...timeStarted[encoder.keys.first].min).cover?(Time.now.min) || (timeStarted[encoder.keys.first].min-20+60..timeStarted[encoder.keys.first].min+60).cover?(Time.now.min)
+            if (timeStarted.min-20...timeStarted.min).cover?(Time.now.min) || (timeStarted.min-20+60..timeStarted.min+60).cover?(Time.now.min)
                 @db.query("update transcoder set in_service=0 where transcoder_id='#{encoder.keys.first}'")
                 @db.query("update uploader set in_service=0 where uploader_id='#{encoder.keys.first}'")
                 @logger.info("taking #{encoder.keys.first} out of service to cool down") if @db.affected_rows > 0 
@@ -111,19 +122,21 @@ def start_encoder(encoder=nil)
     success = false
     if encoder.nil?
         @logger.info("launching new encoder")
-        instance_id=launch_instance(@zone.select {|key, value| value == @zone.values.min }.first[0], @subnets[@zone.min[0][-1]])
+        zone = @zone.select {|key, value| value == @zone.values.min }.first[0]
+        instance_id=launchInstance(zone, @subnets[zone[-1]])
         encoder="enc#{instance_id.gsub("i-", "")}-staging"
         count=0
-        until @db.query("select count(*) from transcoder where transcoder_id='#{encoder}").first.values[0] != 0
+        until @db.query("select count(*) from transcoder where transcoder_id='#{encoder}'").first.values[0] != 0
             count += 1
             sleep 1
-            break if count == 90
+            @logger.info("waiting for #{encoder} to start...") if count % 15 == 0
+            break if count == 300 
         end
         success = true unless @db.query("select count(*) from transcoder where transcoder_id='#{encoder}'").first.values[0] == 0
-        monitor(encoder, true) unless success == false
+        #monitor(encoder, true) unless success == false
     else
         @logger.info("reactivating encoder #{encoder}")
-        @db.query("update transcoder set in_service=1 where transcoder_id='#{encoder.keys.first}'")
+        @db.query("update transcoder set in_service=1 where transcoder_id='#{encoders}'")
         success = true
     end    
     return success
@@ -131,7 +144,11 @@ end
 
 def scaleUp(activateOnly=false)
     response = nil
-    inactive = @db.query("select distinct(transcoder_id) from transcoder where in_service = 0").first.values[0]
+    begin
+       inactive = @db.query("select distinct(transcoder_id) from transcoder where in_service = 0").first.values[0]
+    rescue
+       inactive = []
+    end
     if inactive.size > 0 or activateOnly == true
         response = start_encoder(inactive) 
     elsif inactive.size == 0
@@ -155,6 +172,7 @@ def check_queue
     queue = @db.query("select count(*) from queue where type='conversion' and subpriority < 25 and transcoder_id is null and added < now()-60").first.values[0]
     users = @db.query("SELECT COUNT(DISTINCT `scheduler_group_id`) FROM `queue` WHERE `try_count` < max_try_count;").first.values[0]
     #@db.query("update queue set priority=6 where data not like '%320L%' and subpriority > 25") #prioritize 320L to keep queue at minimum and speed up ready state until proper fix is in place
+    sweeper()
     @logger.debug("queue at #{queue}, users at #{users}")
     if queue > 15 or queuesize > online * 1000
         scaleUp()
