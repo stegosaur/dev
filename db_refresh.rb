8eb9de5f12c751cd7d539a9ac60dd965b4cebed7
Doron Honig <namssa@gmail.com>
2019-02-08 13:37:24 -0500

    Update db_refresh.rb

--- db_refresh.rb
+++ db_refresh.rb
@@ -12,14 +12,26 @@ config = {
   "target_env" => ENV["TARGET_CLUSTER"]
 } 
 
+#hardcode the parameter group to avoid the wrong group being migrated
+instance_parameter_group="auroracustomparameters"
+
 #first create the snapshot
+db_cluster_snapshot_identifier="#{config["db_cluster_identifier"]}-snapshot-#{Time.new.to_i}"
 rds = Aws::RDS::Client.new(:region=>'us-east-1', :credentials => Aws::SharedCredentials.new(:profile_name => "demdex"))
 response = rds.create_db_cluster_snapshot({
   db_cluster_identifier: config["db_cluster_identifier"],
-  db_cluster_snapshot_identifier: "#{config["db_cluster_identifier"]}-snapshot-#{Time.new.to_i}"
+  db_cluster_snapshot_identifier: db_cluster_snapshot_identifier
 })
-sleep 5
 #now share it to another account
+begin
+    while rds.describe_db_cluster_snapshots({db_cluster_snapshot_identifier: db_cluster_snapshot_identifier}).db_cluster_snapshots[0].status != "available"
+          puts "#{Time.now} :: waiting for #{db_cluster_snapshot_identifier} to become ready"
+              sleep 5
+    end
+rescue Exception => e
+    puts "#{Time.now} :: " + e.message
+end
+
 rds.modify_db_cluster_snapshot_attribute({ 
   db_cluster_snapshot_identifier: response.db_cluster_snapshot.db_cluster_snapshot_identifier, 
   attribute_name: "restore", 
@@ -33,36 +45,41 @@ rds.describe_db_cluster_snapshots({
   include_shared: true }).db_cluster_snapshots.each { |snap| $shared_id=snap["db_cluster_snapshot_arn"] if snap.inspect =~/#{response.db_cluster_snapshot.db_cluster_snapshot_identifier}/}
 #now destroy the old cluster
 #
+tags={}
+arn=nil
 rds.describe_db_clusters[:db_clusters].each { |cluster| 
-                                              #first delete the cluster members
-                                              cluster[:db_cluster_members].each { |member| 
-                                                if member[:db_instance_identifier] =~ /#{config["target_env"]}/ 
-                                                  puts "#{Time.now} :: deleting db cluster member #{member[:db_instance_identifier]}"
-                                                  resp = rds.delete_db_instance({ 
-                                                    db_instance_identifier: member[:db_instance_identifier],
-                                                    skip_final_snapshot: true,
-                                                  })
-                                                  $member_config = resp
-                                                end
-                                              }
-                                              sleep 5
-                                              #now wait for them to delete
-                                              begin 
-                                                while rds.describe_db_instances({ db_instance_identifier: $member_config.db_instance.db_instance_identifier }).db_instances[0].db_instance_status == "deleting" do
-                                                 puts "#{Time.now} :: waiting for #{$member_config.db_instance.db_instance_identifier} to delete"
-                                                 sleep 10
-                                                end
-                                                rescue Exception => e
-                                                 puts "#{Time.now} :: " + e.message
-                                              end
-                                              #and delete the cluster once they are gone
-                                              if cluster[:db_cluster_identifier] =~ /#{config["target_env"]}/
-                                                $restore_config = cluster
-                                                rds.delete_db_cluster({
-                                                db_cluster_identifier: cluster[:db_cluster_identifier],
-                                                skip_final_snapshot: true 
-                                                })
-                                              end }
+  #first delete the cluster members
+  cluster[:db_cluster_members].each { |member| 
+    if member[:db_instance_identifier] =~ /va6-#{config["target_env"]}.*aurora-portal/ 
+      arn = cluster.db_cluster_arn
+      rds.list_tags_for_resource({resource_name: arn})[:tag_list].each{|tag| tags.merge!(tag[:key] => tag[:value]) }  # get the tags before deleting
+        puts "#{Time.now} :: deleting db cluster member #{member[:db_instance_identifier]}"
+      resp = rds.delete_db_instance({ 
+        db_instance_identifier: member[:db_instance_identifier],
+      })
+      $member_config = resp
+    end
+  }
+  sleep 5
+  #now wait for them to delete
+  begin 
+    while rds.describe_db_instances({ db_instance_identifier: $member_config.db_instance.db_instance_identifier }).db_instances[0].db_instance_status == "deleting" do
+     puts "#{Time.now} :: waiting for #{$member_config.db_instance.db_instance_identifier} to delete"
+     sleep 10
+    end
+    rescue Exception => e
+     puts "#{Time.now} :: " + e.message
+  end
+  #and delete the cluster once they are gone
+  if cluster[:db_cluster_identifier] =~ /#{config["target_env"]}.*aurora-portal/
+    $restore_config = cluster
+    rds.delete_db_cluster({
+      db_cluster_identifier: cluster[:db_cluster_identifier],
+      skip_final_snapshot: false,
+      final_db_snapshot_identifier: "#{config['target_env']}-cluster-final-snapshot-#{Time.now.to_i}"
+    })
+  end 
+} 
 sleep 5
 
 begin
@@ -89,10 +106,10 @@ while rds.describe_db_clusters({ db_cluster_identifier: $restore_config[:db_clus
 end
 
 #create the cluster members
-puts "#{Time.now} :: creating new db instance in parameter group #{$member_config.db_instance.db_parameter_groups[0].db_parameter_group_name}"
+puts "#{Time.now} :: creating new db instance in parameter group #{instance_parameter_group}"
 rds.create_db_instance ({
     db_instance_identifier: $member_config.db_instance.db_instance_identifier,
-    db_parameter_group_name: $member_config.db_instance.db_parameter_groups[0].db_parameter_group_name,
+    db_parameter_group_name: instance_parameter_group,
     db_instance_class: $member_config.db_instance.db_instance_class,
     engine: $member_config.db_instance.engine,
     db_cluster_identifier: $member_config.db_instance.db_cluster_identifier,
@@ -101,8 +118,9 @@ rds.create_db_instance ({
 
 while rds.describe_db_instances({ db_instance_identifier: $member_config.db_instance.db_instance_identifier }).db_instances[0].db_instance_status == "creating" do
     puts "#{Time.now} :: waiting for #{$member_config.db_instance.db_instance_identifier} to become ready"
-      sleep 10
+    sleep 10
 end
+
 
 #do final modifications
 puts "#{Time.now} :: modifying db cluster: db_cluster_parameter_group_name => #{$restore_config[:db_cluster_parameter_group]}"
@@ -112,19 +130,40 @@ rds.modify_db_cluster({
     db_cluster_identifier:  $restore_config[:db_cluster_identifier],
     db_cluster_parameter_group_name: $restore_config[:db_cluster_parameter_group],
     vpc_security_group_ids: [ $member_config.db_instance.vpc_security_groups[0].vpc_security_group_id ],
+    master_user_password: "sandwich"
 })
 
 sleep 15
 #reboot to ensure they take effect
-puts "#{Time.now} :: rebooting db instance #{$member_config.db_instance.db_instance_identifier}"
+until rds.describe_db_instances({ db_instance_identifier: $member_config.db_instance.db_instance_identifier }).db_instances[0].db_instance_status == "available"
+    puts "#{Time.now} :: waiting for status to become available for reboot. current status: #{rds.describe_db_instances({ db_instance_identifier: $member_config.db_instance.db_instance_identifier }).db_instances[0].db_instance_status}"
+    sleep 5
+end
 rds.reboot_db_instance({
-  db_instance_identifier: $member_config.db_instance.db_instance_identifier,
+  db_instance_identifier: $member_config.db_instance.db_instance_identifier
 })
 
 #don't continue unless the db resolves
+targetEndpoint=rds.describe_db_instances({ db_instance_identifier: $member_config.db_instance.db_instance_identifier }).db_instances[0].endpoint.address
 dbIP = nil
 while dbIP.class != String
-  dbIP = Resolv.getaddress("db") rescue Resolv::ResolvError 
+  dbIP = Resolv.getaddress(targetEndpoint) rescue Resolv::ResolvError 
   puts "#{Time.now} :: waiting for db hostname to resolve"
+  puts "#{targetEndpoint} resolved to #{dbIP}" if dbIP.class != String
   sleep 30
 end
+
+
+puts "#{Time.now} :: rebooting db instance #{$member_config.db_instance.db_instance_identifier} again"
+until rds.describe_db_instances({ db_instance_identifier: $member_config.db_instance.db_instance_identifier }).db_instances[0].db_instance_status == "available"
+    puts "#{Time.now} :: waiting for status to become available for reboot. current status: #{rds.describe_db_instances({ db_instance_identifier: $member_config.db_instance.db_instance_identifier }).db_instances[0].db_instance_status}"
+    sleep 5
+end
+rds.reboot_db_instance({
+    db_instance_identifier: $member_config.db_instance.db_instance_identifier
+}) 
+
+#add the tags back to the cluster
+tags=tags.map{|k,v| { key: k, value: v} }
+rds.add_tags_to_resource({ resource_name: arn, tags: tags })
+rds.add_tags_to_resource({ resource_name: arn.gsub(':cluster:', ':db:').gsub('cluster', '0'), tags: tags })
